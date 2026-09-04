73e6707ae321a11e6478a525592ba92ad92f590f
komrebi32 <hp@DESKTOP-ATD4REN.localdomain>
2026-09-04 19:41:55 -0400

    libs

--- lib/libk/src/serial.asm
+++ lib/libk/src/serial.asm
@@ -2,7 +2,15 @@ section .text
 global serial_putc
 
 serial_putc:
+    push rdx
+    push rax
+.wait:
+    mov dx, 0x3FD
+    in al, dx
+    test al, 0x20
+    jz .wait
+    pop rax
+    pop rdx
     mov dx, 0x3F8
-    mov al, dil
     out dx, al
     ret
