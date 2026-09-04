02f6cf5b8d0bfdf85b63feeee2a04689141c5d97
komrebi32 <hp@DESKTOP-ATD4REN.localdomain>
2026-09-03 21:09:00 -0400

    bootloader new

--- boot/arch/x86_64/bootloader.asm
+++ boot/arch/x86_64/bootloader.asm
@@ -1,52 +1,52 @@
 bits 32
 
+section .multiboot
+align 4
+multiboot_header:
+    dd 0x1BADB002
+    dd 0x00
+    dd -(0x1BADB002 + 0x00)
+
 section .boot
 global start
 extern krnl_main
 
 MBALIGN     equ 1 << 0
 MEMINFO     equ 1 << 1
-FLAGS       equ MBALIGN | MEMINFO
 MAGIC       equ 0x1BADB002
+FLAGS       equ MBALIGN | MEMINFO
 CHECKSUM    equ -(MAGIC + FLAGS)
 
-align 4
-multiboot_header:
-    dd MAGIC
-    dd FLAGS
-    dd CHECKSUM
-
 start:
+    cli
     mov esp, stack_top
 
     call check_cpuid
     call check_long_mode
     call setup_paging
 
-    ; PAE
     mov eax, cr4
-    or eax, 1 << 5
+    or eax, (1 << 5) | (1 << 7)
     mov cr4, eax
 
-    ; Activar modo largo
     mov ecx, 0xC0000080
     rdmsr
-    or eax, 1 << 8
+    or eax, (1 << 8) | (1 << 11)
     wrmsr
 
-    ; Activar la paginacion
+    lgdt [gdt64.pointer]
+
     mov eax, cr0
-    or eax, 1 << 31 | 1 << 0
+    or eax, (1 << 31) | (1 << 0)
     mov cr0, eax
 
-    lgdt [gdt64.pointer]
-    jmp gdt64.code_selector:long_mode_entry
+    jmp 0x08:long_mode_entry
 
 check_cpuid:
     pushfd
     pop eax
     mov ecx, eax
-    xor eax, 1 << 21
+    xor eax, (1 << 21)
     push eax
     popfd
     pushfd
@@ -58,6 +58,7 @@ check_cpuid:
     ret
 .no_cpuid:
     hlt
+    jmp .no_cpuid
 
 check_long_mode:
     mov eax, 0x80000000
@@ -66,11 +67,12 @@ check_long_mode:
     jb .no_long_mode
     mov eax, 0x80000001
     cpuid
-    test edx, 1 << 29
+    test edx, (1 << 29)
     jz .no_long_mode
     ret
 .no_long_mode:
     hlt
+    jmp .no_long_mode
 
 setup_paging:
     mov eax, pdpt_table
@@ -95,9 +97,10 @@ setup_paging:
     mov cr3, eax
     ret
 
+section .text64
 bits 64
 long_mode_entry:
-    mov ax, gdt64.data_selector
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
@@ -111,23 +114,25 @@ long_mode_entry:
     hlt
     jmp .halt
 
-section .bss
-align 4096
-pml4_table: resb 4096
-pdpt_table: resb 4096
-pd_table:   resb 4096
-
-stack_bottom:
-    resb 16384
-stack_top:
-
 section .rodata
 gdt64:
     dq 0
-.code_selector: equ $ - gdt64
-    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)
-.data_selector: equ $ - gdt64
-    dq (1 << 44) | (1 << 47) | (1 << 41)
+.code_selector equ 0x08
+    dq 0x00AF9B000000FFFF
+.data_selector equ 0x10
+    dq 0x00AF93000000FFFF
 .pointer:
-    dw $ - gdt64 - 1
+    dw .pointer - gdt64 - 1
     dq gdt64
+
+section .bss
+align 4096
+pml4_table:
+    resb 4096
+pdpt_table:
+    resb 4096
+pd_table:
+    resb 4096
+stack_bottom:
+    resb 16384
+stack_top:
